5dafda30f790dd50b53a27b0ee522a03cbbfeb77
Joe Vande Vusse <joevandevusse@gmail.com>
2026-03-05 13:00:57 -0600

    Saving a couple SQL updates

--- src/main/sql/updates.sql
+++ src/main/sql/updates.sql
@@ -4,10 +4,19 @@ GRANT ALL PRIVILEGES ON TABLE category_mappings TO your_user;
 GRANT ALL PRIVILEGES ON TABLE user_stats TO your_user;
 GRANT USAGE, SELECT ON SEQUENCE user_stats_id_seq TO your_user;
 
+--truncate category_mappings;
+
+SELECT DISTINCT canonical_topic FROM category_mappings
+                                ORDER BY canonical_topic;
+SELECT COUNT(DISTINCT canonical_topic) FROM category_mappings;
+
 UPDATE clues_java SET answer   = REPLACE(answer,   E'\\"', '"')
                   WHERE answer   LIKE E'%\\"%';
 UPDATE clues_java SET question = REPLACE(question, E'\\"', '"')
                   WHERE question LIKE E'%\\"%';
+UPDATE clues_java SET category   = REPLACE(category,   E'\\"', '"')
+                  WHERE category   LIKE E'%\\"%';
+
 SELECT COUNT(*) FROM clues_java
 WHERE POSITION(E'\\"' IN answer)   > 0
    OR POSITION(E'\\"' IN question) > 0;
